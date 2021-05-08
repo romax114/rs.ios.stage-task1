65111d3c114336791fd09fb8c26a5addfafd77f8
romax114 <70773814+romax114@users.noreply.github.com>
2021-05-08 12:31:57 +0300

    feat: implement function solution

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,15 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    NSInteger n = ABS([number intValue]);
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    NSInteger i = 0;
+    while (n > 0) {
+        result[i] = [NSString stringWithFormat:@"%ld", n % 10];
+        n = n / 10;
+        i++;
+    }
+    return result;
 }
 
 @end
