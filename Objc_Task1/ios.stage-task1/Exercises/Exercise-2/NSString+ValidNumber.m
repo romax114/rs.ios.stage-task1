48f9c260411dbf9d69285f3e8685f37ebbe8e2b3
romax114 <70773814+romax114@users.noreply.github.com>
2021-05-08 12:27:45 +0300

    implement function solution

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -3,7 +3,22 @@
 @implementation NSString (ValidNumber)
 
 - (BOOL)isValidNumber {
-    return false;
+    NSUInteger length = [self length];
+    
+    if (length <= 0 || length > 100) {
+        return NO;
+    }
+    
+    BOOL result = YES;
+    
+    for (int i = 0; i < length; i++) {
+        if (![[NSCharacterSet decimalDigitCharacterSet] characterIsMember: [self characterAtIndex:i]]) {
+            result = NO;
+            break;
+        }
+    }
+    
+    return result;
 }
 
 @end
